31ee3500e8eff9172c8b30615a72bca9a8b4d535
KaushalKumarDubey <kaushal@eloop.dev>
2025-03-28 14:27:05 +0530

    Login via Phone no and Pasword

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const { signup, sendOtp, verifyOtp, logout } = require("../controllers/auth");
+const { signup, login, logout } = require("../controllers/auth");
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/send-otp", sendOtp);
-router.post("/verify-otp", verifyOtp);
+router.post("/login", login);
 router.post("/logout", logout);
 
 module.exports = router;
-
